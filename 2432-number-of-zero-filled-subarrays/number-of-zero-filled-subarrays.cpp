baa50c27590ee4d89769cc17173d5f874b7aeb85
Sanskriti Bajpai <116370233+Sanskriti-Bajpai-dev@users.noreply.github.com>
2025-08-19 22:11:54 +0530

    Time: 0 ms (100.00%) | Memory: 111.2 MB (74.50%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
         long long cnt =0;
-        long long streak=0;
+        long streak=0;
         for(int num:nums){
             if(num==0){
                 streak++;
